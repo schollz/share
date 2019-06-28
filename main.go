952b9e2993b25564eea328810bcad6ffb2028e44
Zack Scholl <zack.scholl@gmail.com>
2019-06-28 08:13:15 -0700

    q!

--- main.go
+++ main.go
@@ -450,12 +450,16 @@ func handle(w http.ResponseWriter, r *http.Request) (err error) {
 		return p.handleGetHome(w, r)
 	} else if r.Method == "GET" && strings.HasPrefix(r.URL.Path, "/exists/") {
 		urlPathSplit := strings.Split(r.URL.Path, "/")
-		id := urlPathSplit[len(urlPathSplit)-1]
-		_, errStat := os.Stat(path.Join(c.ContentDirectory, id))
+		if len(urlPathSplit) < 3 {
+			jsonResponse(w, http.StatusOK, map[string]string{"exists": "yes", "id": "", "name": ""})
+		}
+		id := filepath.Clean(urlPathSplit[len(urlPathSplit)-2])
+		name := filepath.Clean(urlPathSplit[len(urlPathSplit)-1])
+		_, errStat := os.Stat(path.Join(c.ContentDirectory, id, name))
 		if errStat != nil {
-			jsonResponse(w, http.StatusOK, map[string]bool{"exists": false})
+			jsonResponse(w, http.StatusOK, map[string]string{"exists": "no", "id": id, "name": name})
 		} else {
-			jsonResponse(w, http.StatusOK, map[string]bool{"exists": true})
+			jsonResponse(w, http.StatusOK, map[string]string{"exists": "yes", "id": id, "name": name})
 		}
 		return nil
 	} else if r.Method == "GET" && strings.HasPrefix(r.URL.Path, "/static/") {
