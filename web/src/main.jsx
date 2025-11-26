7486ed2aa21baf392ec417fdf0bf8ac07de68d9b
Zack Scholl <zack.scholl@gmail.com>
2025-11-26 07:35:49 -0800

    fix: router

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -31,6 +31,7 @@ createRoot(document.getElementById("root")).render(
                     <Route path="/login" element={<Login />} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/share/:token" element={<SharedFile />} />
+                    <Route path="/:room" element={<Landing />} />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
